e0e8bd6a230577bf0bccf862f148d51c5dc295b6
Callahan <95487773+Call4han@users.noreply.github.com>
2022-08-01 17:54:22 -0500

    commit for Koigui

---
+++ src/main/java/me/callahandev/chapter46/Chapter46.java
@@ -0,0 +1,14 @@
+package me.callahandev.chapter46;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Chapter46 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+
+}
